15d2ad9939a28e2bd042abd3a71fa850e6cfb83c
Finn-TH <87751347+Finn-TH@users.noreply.github.com>
2024-12-11 14:43:40 +0800

    Added cart page, cart button navigation

--- ddac-bookmate/Controllers/CartController.cs
+++ ddac-bookmate/Controllers/CartController.cs
@@ -58,5 +58,53 @@ namespace ddac_bookmate.Controllers
             }
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(int bookId)
+        {
+            var userId = User.Identity.Name;
+            var cart = await _context.Carts
+                .Include(c => c.BookCarts)
+                .FirstOrDefaultAsync(c => c.UserId == userId);
+
+            // Create cart if it doesn't exist
+            if (cart == null)
+            {
+                cart = new Cart { UserId = userId };
+                _context.Carts.Add(cart);
+                await _context.SaveChangesAsync();
+            }
+
+            // Get book details
+            var book = await _context.Books.FindAsync(bookId);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            // Check if book already in cart
+            var existingItem = cart.BookCarts?
+                .FirstOrDefault(bc => bc.BookId == bookId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += 1;
+            }
+            else
+            {
+                // Add new book to cart
+                var bookCart = new BookCart
+                {
+                    BookId = bookId,
+                    CartId = cart.CartId,
+                    Quantity = 1,
+                    UnitPrice = book.BookPrice
+                };
+                _context.BookCarts.Add(bookCart);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Cart");
+        }
     }
 } 
